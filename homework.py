5a79c3c1db2e2b8266a337ea851250553a91d312
morgunovvb <74514540+morgunovvb@users.noreply.github.com>
2022-03-28 20:28:57 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -16,7 +16,7 @@ load_dotenv()
 
 PRACTICUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 300
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
